e028db56fc3bf1f7e28b6b58f8a9d19edcfe57cc
Mark Edgar <medgar123@gmail.com>
2011-10-26 09:37:08 +0200

    Remove readlink -f from test scripts. Clean up tokenizer-test.sh Hide successful test output.

--- test/invalid-test.sh
+++ test/invalid-test.sh
@@ -1,15 +1,7 @@
 #! /usr/bin/env bash
 
-__filename=`readlink -f $0`
-__dirname=`dirname $__filename`
-cd $__dirname
+cd ${0%/*}
 
-echo  _=$_
-echo  0=$0
-echo __filename=$__filename
-echo __dirname=$__dirname
-echo  PWD=$PWD
-#env
 fails=0
 for input in invalid/*
 do
@@ -20,11 +12,6 @@ do
     cat outlog
     echo "<<<"
     let fails=$fails+1
-#  else
-#    echo "OK: cat $input | ../bin/json_parse failed correctly"
-#    echo "stderr was >>>"
-#    cat errlog
-#    echo "<<<"
   fi
 done
 echo "$fails test(s) failed"
