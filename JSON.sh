9974db50010fb9b379905f29aa33c5cd4e06921c
Jim Klimov <jim@jimklimov.com>
2015-01-06 21:18:38 +0100

    Added support for sorting contents of the JSON markup, for printing out "normalized JSON markup" (the top element only, without the bracketed header), for concatenating input string values with embedded newlines into single-line strings with embedded "\n"

--- JSON.sh
+++ JSON.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# https://github.com/dominictarr/JSON.sh/blob/master/JSON.sh
+# MIT / Apache 2 licenses (C) 2014 by "dominictarr" checked out 2015-01-04
+# further development (C) 2015 Jim Klimov <EvgenyKlimov@eaton.com>
+
 throw () {
   echo "$*" >&2
   exit 1
@@ -8,15 +12,31 @@ throw () {
 BRIEF=0
 LEAFONLY=0
 PRUNE=0
+SORTDATA=""
+NORMALIZE=0
 
 usage() {
   echo
-  echo "Usage: JSON.sh [-b] [-l] [-p] [-h]"
+  echo "Usage: JSON.sh [-b] [-l] [-p] [-N] [-S|-S='args']"
+  echo "       JSON.sh [-N|-N='args'] < markup.json"
+  echo "       JSON.sh [-h]"
+  echo "-h - This help text."
   echo
   echo "-p - Prune empty. Exclude fields with empty values."
   echo "-l - Leaf only. Only show leaf nodes, which stops data duplication."
   echo "-b - Brief. Combines 'Leaf only' and 'Prune empty' options."
-  echo "-h - This help text."
+  echo
+  echo "Sorting is also available, although limited to single-line strings in"
+  echo "the markup (multilines are automatically escaped into backslash+n):"
+  echo "-S - Sort the contents of items in JSON markup and leaf-list markup:"
+  echo "     'sort' objects by key names and then values, and arrays by values"
+  echo "-S='args' - use 'sort \$args' for content sorting, e.g. use -S='-n -r'"
+  echo "     for reverse numeric sort"
+  echo "-N - Normalize the input JSON markup into a single-line JSON output;"
+  echo "     in this mode syntax and spacing are normalized, data order remains"
+  echo "-N='args' - Normalize the input JSON markup into a single-line JSON"
+  echo "     output with contents sorted like for -S='args', e.g. use -N='-n'"
+  echo "     This is equivalent to -N -S='args', just more compact to write"
   echo
 }
 
@@ -25,7 +45,7 @@ parse_options() {
   local ARGN=$#
   while [ $ARGN -ne 0 ]
   do
-    case $1 in
+    case "$1" in
       -h) usage
           exit 0
       ;;
@@ -37,6 +57,15 @@ parse_options() {
       ;;
       -p) PRUNE=1
       ;;
+      -N) NORMALIZE=1
+      ;;
+      -N=*) SORTDATA="sort `echo "$1" | sed 's,^-N=,,'`"
+          NORMALIZE=1
+      ;;
+      -S) SORTDATA="sort"
+      ;;
+      -S=*) SORTDATA="sort `echo "$1" | sed 's,^-S=,,'`"
+      ;;
       ?*) echo "ERROR: Unknown option."
           usage
           exit 0
@@ -45,6 +74,9 @@ parse_options() {
     shift 1
     ARGN=$((ARGN-1))
   done
+
+  # For normalized data, we do the whole job and just return the top object
+  [ "$NORMALIZE" -eq 1 ] && BRIEF=0 && LEAFONLY=0 && PRUNE=0
 }
 
 awk_egrep () {
@@ -58,6 +90,49 @@ awk_egrep () {
       $0=substr($0, start+RLENGTH);
     }
   }' pattern=$pattern_string
+}
+
+strip_newlines() {
+  # replace line returns inside strings in input with \n string
+  local ILINE
+  local LINESTRIP
+  local NUMQ
+  local ODD
+  local INSTRING=0
+
+  # the first "grep" should ensure that input has a trailing newline
+  grep '' | while IFS="" read -r ILINE; do
+    # Remove escaped quotes:
+    LINESTRIP="${ILINE//\\\"}"
+    # Remove all chars but remaining quotes:
+    LINESTRIP="${LINESTRIP//[^\"]}"
+    # Count unescaped quotes:
+    NUMQ="${#LINESTRIP}"
+    ODD="$(($NUMQ%2))"
+
+    if [ "$ODD" -eq 1 -a "$INSTRING" -eq 0 ]; then
+      printf '%s\\n' "$ILINE"
+      INSTRING=1
+      continue
+    fi
+
+    if [ "$ODD" -eq 1 -a "$INSTRING" -eq 1 ]; then
+      printf '%s\n' "$ILINE"
+      INSTRING=0
+      continue
+    fi
+
+    if [ "$ODD" -eq 0 -a "$INSTRING" -eq 1 ]; then
+      printf '%s\\n' "$ILINE"
+      continue
+    fi
+
+    if [ "$ODD" -eq 0 -a "$INSTRING" -eq 0 ]; then
+      printf '%s\n' "$ILINE"
+      continue
+    fi
+  done
+  :
 }
 
 tokenize () {
@@ -82,17 +157,20 @@ tokenize () {
     CHAR='[^[:cntrl:]"\\\\]'
   fi
 
-  local STRING="\"$CHAR*($ESCAPE$CHAR*)*\""
+  local STRINGVAL="$CHAR*($ESCAPE$CHAR*)*"
+  local STRING="(\"$STRINGVAL\")"
   local NUMBER='-?(0|[1-9][0-9]*)([.][0-9]*)?([eE][+-]?[0-9]*)?'
   local KEYWORD='null|false|true'
   local SPACE='[[:space:]]+'
 
+  strip_newlines | \
   $GREP "$STRING|$NUMBER|$KEYWORD|$SPACE|." | egrep -v "^$SPACE$"
 }
 
 parse_array () {
   local index=0
   local ary=''
+  local aryml=''
   read -r token
   case "$token" in
     ']') ;;
@@ -101,7 +179,11 @@ parse_array () {
       do
         parse_value "$1" "$index"
         index=$((index+1))
-        ary="$ary""$value" 
+        ary="$ary""$value"
+        if [ -n "$SORTDATA" ]; then
+            [ -z "$aryml" ] && aryml="$value" || aryml="$aryml
+$value"
+        fi
         read -r token
         case "$token" in
           ']') break ;;
@@ -112,6 +194,9 @@ parse_array () {
       done
       ;;
   esac
+  if [ -n "$SORTDATA" ]; then
+    ary="`echo "$aryml" | $SORTDATA | tr '\n' ',' | sed 's|,*$||' | sed 's|^,*||'`"
+  fi
   [ "$BRIEF" -eq 0 ] && value=`printf '[%s]' "$ary"` || value=
   :
 }
@@ -119,6 +204,7 @@ parse_array () {
 parse_object () {
   local key
   local obj=''
+  local objml=''
   read -r token
   case "$token" in
     '}') ;;
@@ -136,7 +222,11 @@ parse_object () {
         esac
         read -r token
         parse_value "$1" "$key"
-        obj="$obj$key:$value"        
+        obj="$obj$key:$value"
+        if [ -n "$SORTDATA" ]; then
+            [ -z "$objml" ] && objml="$key:$value" || objml="$objml
+$key:$value"
+        fi
         read -r token
         case "$token" in
           '}') break ;;
@@ -147,6 +237,9 @@ parse_object () {
       done
     ;;
   esac
+  if [ -n "$SORTDATA" ]; then
+    obj="`echo "$objml" | $SORTDATA | tr '\n' ',' | sed 's|,*$||' | sed 's|^,*||'`"
+  fi
   [ "$BRIEF" -eq 0 ] && value=`printf '{%s}' "$obj"` || value=
   :
 }
@@ -163,12 +256,22 @@ parse_value () {
        [ "$value" = '""' ] && isempty=1
        ;;
   esac
+
+  if [ "$NORMALIZE" -eq 1 ]; then
+    # Ensure a "true" output from the "if" for "return"
+    [ "$jpath" != '' ] || printf "%s\n" "$value"
+    return
+  fi
+
+  ### Skip printing larger objects in brief mode
   [ "$value" = '' ] && return
+
   [ "$LEAFONLY" -eq 0 ] && [ "$PRUNE" -eq 0 ] && print=1
   [ "$LEAFONLY" -eq 1 ] && [ "$isleaf" -eq 1 ] && [ $PRUNE -eq 0 ] && print=1
   [ "$LEAFONLY" -eq 0 ] && [ "$PRUNE" -eq 1 ] && [ "$isempty" -eq 0 ] && print=1
   [ "$LEAFONLY" -eq 1 ] && [ "$isleaf" -eq 1 ] && \
     [ $PRUNE -eq 1 ] && [ $isempty -eq 0 ] && print=1
+
   [ "$print" -eq 1 ] && printf "[%s]\t%s\n" "$jpath" "$value"
   :
 }
@@ -183,8 +286,17 @@ parse () {
   esac
 }
 
+smart_parse() {
+  tokenize | if [ -n "$SORTDATA" ] ; then
+      ( NORMALIZE=1 LEAFONLY=0 BRIEF=0 parse ) \
+      | tokenize | parse
+    else
+      parse
+    fi
+}
+
 if ([ "$0" = "$BASH_SOURCE" ] || ! [ -n "$BASH_SOURCE" ]);
 then
   parse_options "$@"
-  tokenize | parse
+  smart_parse
 fi
