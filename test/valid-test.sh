e04de1a8467539d1bff007c78ac66c9ea687761c
Jim Klimov <jim@jimklimov.com>
2015-01-07 01:45:39 +0100

    Added tests for sorting (with multiple parameters) and normalization

--- test/valid-test.sh
+++ test/valid-test.sh
@@ -1,21 +1,34 @@
 #! /usr/bin/env bash
 
+# To disambiguate tests on sorting, use one locale
+LANG=C
+LC_ALL=C
+export LANG LC_ALL
+
 cd ${0%/*}
 fails=0
 i=0
 tests=`ls valid/*.json -1l | wc -l`
+tests=$(($tests*3))
 echo "1..$tests"
 for input in valid/*.json
 do
-  expected="${input%.json}.parsed"
-  i=$((i+1))
-  if ! ../JSON.sh < "$input" | diff -u - "$expected" 
-  then
-    echo "not ok $i - $input"
-    fails=$((fails+1))
-  else
-    echo "ok $i - $input"    
-  fi
+  for EXT in parsed sorted normalized; do
+    expected="${input%.json}.$EXT"
+    i=$((i+1))
+    case "$EXT" in
+      sorted) OPTIONS="-S='-n -r'" ;;
+      normalized) OPTIONS="-N" ;;
+      parsed|*) OPTIONS="" ;;
+    esac
+    if ! eval ../JSON.sh $OPTIONS < "$input" | diff -u - "$expected" 
+    then
+      echo "not ok $i - $input $EXT"
+      fails=$((fails+1))
+    else
+      echo "ok $i - $input $EXT"
+    fi
+  done
 done
 echo "$fails test(s) failed"
 exit $fails
