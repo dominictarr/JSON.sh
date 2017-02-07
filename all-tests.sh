278611f3fec9af2c81751edd963230040ef329b7
Jim Klimov <jim@jimklimov.com>
2017-02-07 18:48:09 +0100

    all-tests.sh : report which shells were tested and how they performed

--- all-tests.sh
+++ all-tests.sh
@@ -44,6 +44,9 @@ jsonsh_tests() (
     exit $exitcode
 )
 
+OKAY_SHELLS=""
+FAIL_SHELLS=""
+SKIP_SHELLS=""
 [ -n "$SHELL_PROGS" ] || SHELL_PROGS="bash dash ash busybox ksh ksh88 ksh93"
 for SHELL_PROG in $SHELL_PROGS ; do
     [ "$SHELL_PROG" = "busybox" ] && SHELL_PROG="busybox sh"
@@ -53,6 +56,7 @@ for SHELL_PROG in $SHELL_PROGS ; do
     if [ -n "$SHELL_PROG" ] ; then
         if $SHELL_PROG -c "date" >/dev/null 2>&1 ; then : ; else
             echo "SKIP missing shell : $SHELL_PROG"
+            SKIP_SHELLS="$SKIP_SHELLS $SHELL_PROG"
             continue
         fi
         export SHELL_PROG
@@ -62,8 +66,13 @@ for SHELL_PROG in $SHELL_PROGS ; do
         echo "TESTING WITH default shell interpreter e.g. likely with /bin/sh, whatever this is in your OS"
     fi
 
-    jsonsh_tests || overall_exitcode=$?
+    jsonsh_tests && OKAY_SHELLS="$OKAY_SHELLS $SHELL_PROG" || \
+        { overall_exitcode=$? ; FAIL_SHELLS="$FAIL_SHELLS $SHELL_PROG" ; }
     echo ""
 done
 
+echo "OVERALL RESULT:"
+echo "OKAY_SHELLS = $OKAY_SHELLS"
+echo "FAIL_SHELLS = $FAIL_SHELLS"
+echo "SKIP_SHELLS = $SKIP_SHELLS"
 exit $overall_exitcode
