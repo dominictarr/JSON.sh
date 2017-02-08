0c7dd88b275dca5ba8bfeeadd6076346a335aa84
Jim Klimov <jim@jimklimov.com>
2017-02-08 11:57:11 +0100

    solidus-test.sh : track number of test

--- test/solidus-test.sh
+++ test/solidus-test.sh
@@ -13,23 +13,26 @@ OUTPUT_WITHOUT_ESCAPING=./solidus/string_with_solidus.no-escaping.parsed
 FAILS=0
 
 echo "1..2"
+i=0
 
 # Such explicit chaining is equivalent to "pipefail" in non-Bash interpreters
-JSONSH_OUT="$(jsonsh_cli < "$INPUT")" && \
+i="$(expr $i + 1)"
+JSONSH_OUT="$(eval jsonsh_cli < "$INPUT")" && \
     printf '%s\n' "$JSONSH_OUT" | diff -u - "${OUTPUT_ESCAPED}"
 JSONSH_RES=$?
 if [ "$JSONSH_RES" != 0 ] ; then
-  echo "not ok - JSON.sh run without -s option should leave solidus escaping intact"
+  echo "not ok $i - JSON.sh run without -s option should leave solidus escaping intact"
   FAILS="$(expr $FAILS + 1)"
 else
   echo "ok $i - solidus escaping was left intact"
 fi
 
-JSONSH_OUT="$(jsonsh_cli -s < "$INPUT")" && \
+i="$(expr $i + 1)"
+JSONSH_OUT="$(eval jsonsh_cli -s < "$INPUT")" && \
     printf '%s\n' "$JSONSH_OUT" | diff -u - "${OUTPUT_WITHOUT_ESCAPING}"
 JSONSH_RES=$?
 if [ "$JSONSH_RES" != 0 ] ; then
-  echo "not ok - JSON.sh run with -s option should remove solidus escaping"
+  echo "not ok $i - JSON.sh run with -s option should remove solidus escaping"
   FAILS="$(expr $FAILS + 1)"
 else
   echo "ok $i - solidus escaping has been removed"
